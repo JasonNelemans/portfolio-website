9b02073acde3cdacd6301db7b2c122bf11e623a2
JasonNelemans <jasonnelemans@hotmail.com>
2020-09-08 10:33:54 +0200

    created simple footer, added simple global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import styled, { createGlobalStyle } from 'styled-components';
 
-import About from './pages/about'
+import About from './pages/about';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={About} />
-      </Switch>
+      <GlobalStyles />
+      <Wrapper>
+        <Content>
+          <Switch>
+            <Route exact path="/" component={About} />
+          </Switch>
+        </Content>
+        <Footer />
+      </Wrapper>
     </div>
   );
 }
+
+export const GlobalStyles = createGlobalStyle`
+  html: 100%;
+  body {
+    margin: 0;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
+const Content = styled.div`
+  flex-grow: 1;
+`
 
 export default App;
